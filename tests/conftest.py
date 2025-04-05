eb35127ef2c0eb71de0c1b0bd1ba44448106103a
Jeries Mazzawi <jeriesmazzawi@Jeriess-MacBook-Air.local>
2025-04-05 15:57:44 +0300

    option to choose which engine to use (pw/selenium)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,42 +1,61 @@
 import pytest
-from playwright.sync_api import Page, sync_playwright
+from playwright.sync_api import sync_playwright
 from selenium import webdriver
 
 
-@pytest.fixture(scope='class')
+def pytest_addoption(parser):
+    parser.addoption("--engine", action="store", default="selenium",
+                     help="Choose between 'selenium' or 'playwright' as the browser engine.")
+
+
+def pytest_collection_modifyitems(config, items):
+    engine = config.getoption("--engine")
+
+    if engine == "playwright":
+        skip_selenium = pytest.mark.skip(reason="Skipped because engine is playwright")
+        for item in items:
+            if "playwright" not in item.keywords:
+                item.add_marker(skip_selenium)
+
+    elif engine == "selenium":
+        skip_playwright = pytest.mark.skip(reason="Skipped because engine is selenium")
+        for item in items:
+            if "selenium" not in item.keywords:
+                item.add_marker(skip_playwright)
+
+
+@pytest.fixture(scope="class", autouse=True)
+def driver(request):
+    engine = request.config.getoption("--engine")
+
+    if engine == "selenium":
+        driver = selenium_driver()
+        yield driver
+        driver.close()
+        driver.quit()
+    elif engine == "playwright":
+        with sync_playwright() as p:
+            driver, browser = browser_instance(p)
+            yield driver
+            driver.close()
+            browser.close()
+    else:
+        raise ValueError(f"Unsupported engine '{engine}'. Please use 'selenium' or 'playwright'.")
+
+
 def selenium_driver():
     driver = webdriver.Chrome()
     driver.get("https://www.saucedemo.com/")
     driver.maximize_window()
-
-    yield driver
-    driver.quit()
+    return driver
 
 
-@pytest.fixture(scope="session")
-def playwright_driver(page: Page):
+def browser_instance(p):
     """
-    launching the browser handled by the plugin -> Playwright-Pytest plugin (pytest-playwright).
-    No browser close logic is needed, Playwright handles it under the hood.
+    Creates a Chromium browser instance manually using Playwright.
+    Returns a page object and browser instance for more controlled management.
     """
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
     page.goto("https://www.saucedemo.com/")
-    yield
-
-
-@pytest.fixture(scope="class")
-def browser_instance():
-    """
-    here we manually manage the browser lifecycle
-    - Creates a Chromium browser instance manually.
-    - Returns a page object after navigation.
-    - Need to close the browser after the tests.
-
-     when to use manual setup?
-     when u want more control over the browser, like setting options or custom browser contexts.
-    """
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto("https://www.saucedemo.com/")
-        yield page
-        browser.close()
+    return page, browser
