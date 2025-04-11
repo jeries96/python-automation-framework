a283ce6bb3de6d72e2440f781802c82c7191b777
Jeries Mazzawi <jeriesmazzawi@Jeriess-MacBook-Air.local>
2025-04-11 13:48:54 +0300

    add allure steps on methods

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 
 from pages.base_page import BaseElement
@@ -12,18 +13,23 @@ class LoginPage(BaseElement):
     def __init__(self, driver):
         super().__init__(driver)
 
+    @allure.step
     def fill_username(self, username: str):
         self.send_keys(self.username_input_locator, value=username)
 
+    @allure.step
     def fill_password(self, password: str):
         self.send_keys(self.password_input_locator, value=password)
 
+    @allure.step
     def click_login(self):
         self.click(self.login_button_locator)
 
+    @allure.step
     def is_error_displayed(self):
         return self.wait_for_visibility(self.login_error_text_alert)
 
+    @allure.step
     def login(self, username, password):
         self.fill_username(username)
         self.fill_password(password)
--- pages/login_page_pw.py
+++ pages/login_page_pw.py
@@ -1,3 +1,4 @@
+import allure
 from playwright.sync_api import Page
 
 
@@ -9,18 +10,23 @@ class LoginPagePlayWright:
         self.login_button_locator = page.get_by_role("button", name="Login")
         self.error_text_locator = page.locator("css=h3[data-test='error']")
 
+    @allure.step
     def fill_username(self, username: str):
         self.username_input_locator.fill(username)
 
+    @allure.step
     def fill_password(self, password: str):
         self.password_input_locator.fill(password)
 
+    @allure.step
     def click_login(self):
         self.login_button_locator.click()
 
+    @allure.step
     def is_error_displayed(self):
         return self.error_text_locator.is_visible()
 
+    @allure.step
     def login(self, username, password):
         self.fill_username(username)
         self.fill_password(password)
